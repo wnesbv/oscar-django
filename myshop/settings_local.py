9d63e4b93af5b7792729b220353c165433f2d582
wnesbv <92110749+wnesbv@users.noreply.github.com>
2021-11-27 10:14:40 +0300

    Add files via upload

---
+++ myshop/settings_local.py
@@ -0,0 +1,15 @@
+
+
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+DEBUG = True
+ALLOWED_HOSTS =  ['.localhost', '127.0.0.1', '[::1]']
+# ...
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
